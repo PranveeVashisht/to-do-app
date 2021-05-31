7e9aedb83fc3c24a1f8d43d6aad42ea9001406b2
rdotjain <rupanshijain45678@gmail.com>
2021-06-01 01:29:44 +0530

    Setup for deployment

---
+++ todo/settings/dev.py
@@ -0,0 +1,17 @@
+from todo.settings.base import *
+import environ
+import os
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+# reading .env file
+environ.Env.read_env(env_file=os.path.join(BASE_DIR, "./.env"))
+
+# False if not in os.environ
+DEBUG = env("DEBUG")
+
+# Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
+SECRET_KEY = env('SECRET_KEY')
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
---
+++ todo/settings/prod.py
@@ -0,0 +1,15 @@
+from todo.settings.base import *
+import os
+import dj_database_url
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY", 'p7g_g$a7^qfn!zxc&h+fbvy*0+@trz7)pkr#54elu!1u#rg@tq')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+
+ALLOWED_HOSTS = []
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
