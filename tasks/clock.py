266d5819f73b6178fdeeb78585100ae2962136c1
rdotjain <rupanshijain45678@gmail.com>
2021-07-11 19:19:40 +0530

    moved clock to tasks

---
+++ tasks/clock.py
@@ -0,0 +1,12 @@
+import json, requests
+from apscheduler.schedulers.blocking import BlockingScheduler
+sched = BlockingScheduler()
+
+def my_scheduled_job():
+    """Function to fetch json data from github api periodically"""
+    response = requests.get("https://api.github.com/orgs/Servatom/repos")
+    with open("api.json", mode='w') as file:
+        json.dump(response.json(), file)
+
+sched.add_job(my_scheduled_job, 'interval', minutes=2)
+sched.start()
