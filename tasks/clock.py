6de30c31a7c1a51c6fcd4e807bf975bcd325d2da
rdotjain <rupanshijain45678@gmail.com>
2021-07-11 17:20:10 +0530

    forgotten commit

--- tasks/clock.py
+++ tasks/clock.py
@@ -1,13 +1,12 @@
 import json, requests
-# from apscheduler.schedulers.blocking import BlockingScheduler
-# sched = BlockingScheduler()
+from apscheduler.schedulers.blocking import BlockingScheduler
+sched = BlockingScheduler()
 
-# @sched.scheduled_job('interval', minutes=3)
+@sched.scheduled_job('interval', minutes=3)
 def my_scheduled_job():
     """Function to fetch json data from github api periodically"""
     response = requests.get("https://api.github.com/users/rdotjain/repos")
     with open("api.json", mode='w') as file:
         json.dump(response.json(), file)
 
-# sched.start()
-my_scheduled_job()
+sched.start()
