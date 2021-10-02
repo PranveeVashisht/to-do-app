0e501f93f58a1241367cc3e68555bb996e7f26a8
rdotjain <rupanshijain45678@gmail.com>
2021-10-02 14:40:15 +0530

    ready for deployment pt.2

--- tasks/apps.py
+++ tasks/apps.py
@@ -3,8 +3,3 @@ from django.apps import AppConfig
 
 class TasksConfig(AppConfig):
     name = 'tasks'
-
-    def ready(self):
-        print("Running")
-        from . import cron
-        cron.create_sample_tasks()
