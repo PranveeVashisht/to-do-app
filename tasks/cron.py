ad014aa81fb8f5d26ebf402d4bf4604144f1489e
rdotjain <rupanshijain45678@gmail.com>
2021-10-02 15:06:12 +0530

    directory structured

--- tasks/cron.py
+++ tasks/cron.py
@@ -3,10 +3,10 @@ from tasks.models import Task
 
 
 def create_sample_tasks():
-    os.chdir("tasks/sample_tasks")
+    os.chdir("sample_tasks")
     for file in os.listdir():
         if file.endswith(".txt"):
             with open(file, "r") as f:
                 task_text = f.read()
                 Task.objects.update_or_create(title=task_text)
-    os.chdir("../..")
+    os.chdir("..")
